9ba9946e5e9c31768c5c99aac400e0322b7a82ff
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2026-02-11 13:49:13 -0500

    devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = false
