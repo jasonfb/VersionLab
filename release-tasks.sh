cfa53269fccbe5474dddd0bf43707818b5f5e864
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2026-02-17 13:39:35 -0500

    fix for release-tasks.sh

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,9 @@
 # Step to execute
 bundle exec rails db:migrate data:migrate
 # check for a good exit
-if [ 0 -ne 0 ]
+if [ $? -ne 0 ]
 then
-  puts '*** RELEASE COMMAND FAILED'
+  echo '*** RELEASE COMMAND FAILED'
   # something went wrong; convey that and exit
   exit 1
 fi
