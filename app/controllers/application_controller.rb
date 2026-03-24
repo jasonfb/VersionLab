3fb8af2e3f3225ae7165a36660a65cc991e554f0
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2026-03-23 23:30:04 -0400

    protecting site with 'username' and 'manyversions'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: "username", password: "manyversions"
+
   include Pundit::Authorization
 
   include Pagy::Method
