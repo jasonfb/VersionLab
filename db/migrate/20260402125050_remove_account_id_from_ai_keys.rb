72f4f97a3a043f82ef210ca1bd07c7c3ab1c6218
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2026-04-07 18:17:21 -0400

    removing all account specific keys

--- db/migrate/20260402125050_remove_account_id_from_ai_keys.rb
+++ db/migrate/20260402125050_remove_account_id_from_ai_keys.rb
@@ -1,7 +1,14 @@
 class RemoveAccountIdFromAiKeys < ActiveRecord::Migration[8.1]
-  def change
+  def up
     remove_index :ai_keys, name: :index_ai_keys_on_account_id_and_ai_service_id
-    remove_column :ai_keys, :account_id, :uuid, null: false
+    execute "DELETE FROM ai_keys"
+    remove_column :ai_keys, :account_id, :uuid
     add_index :ai_keys, :ai_service_id, unique: true
+  end
+
+  def down
+    remove_index :ai_keys, :ai_service_id
+    add_column :ai_keys, :account_id, :uuid, null: true
+    add_index :ai_keys, [:account_id, :ai_service_id], name: :index_ai_keys_on_account_id_and_ai_service_id
   end
 end
