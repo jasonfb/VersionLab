7db7cbe4d79cd3e5256be890e070f6e7d9ccd92a
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2026-04-15 13:03:48 -0400

    removes basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "username", password: "manyversions", if: -> { Rails.env.production? }
-
   include Pundit::Authorization
 
   include Pagy::Method
